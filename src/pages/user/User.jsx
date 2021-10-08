25aae13504f90d2fbb655fcf2e408032b8dfb9d0
Donskilful <donskilful@gmail.com>
2021-10-08 08:28:38 +0100

    Update user page completed

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,11 +1,123 @@
-import "./user.css"
+import {
+  CalendarToday,
+  LocationSearching,
+  MailOutline,
+  PermIdentity,
+  PhoneAndroid,
+  Publish,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import "./user.css";
 
 const User = () => {
-    return (
-        <div className="user">
-            User
+  return (
+    <div className="user">
+      <div className="userTitleContainer">
+        <h1 className="userTitle">Edit User</h1>
+        <Link to="/newUser">
+        <button className="userAddButton">Create</button>
+        </Link>
+      </div>
+      <div className="userContainer">
+        <div className="userShow">
+          <div className="userShowTop">
+            <img
+              src="https://www.attitudestatus.org/wp-content/uploads/2021/05/pic-girls-294x300.jpg"
+              alt=""
+              className="userShowImg"
+            />
+            <div className="userShowTopTitle">
+              <span className="userShowUsername">Ann Becka</span>
+              <span className="userShowUserTitle">Software Engineer</span>
+            </div>
+          </div>
+          <div className="userShowBottom">
+            <span className="userShowTitle">Account Details</span>
+            <div className="userShowInfo">
+              <PermIdentity className="userShowIcon" />
+              <span className="userShowInfoTitle">annabeck4</span>
+            </div>
+            <div className="userShowInfo">
+              <CalendarToday className="userShowIcon" />
+              <span className="userShowInfoTitle">04.16.1998</span>
+            </div>
+            <span className="userShowTitle">Contact Details</span>
+            <div className="userShowInfo">
+              <PhoneAndroid className="userShowIcon" />
+              <span className="userShowInfoTitle">+1 686 8669 342</span>
+            </div>
+            <div className="userShowInfo">
+              <MailOutline className="userShowIcon" />
+              <span className="userShowInfoTitle">annabeck4@yahoo.com</span>
+            </div>
+            <div className="userShowInfo">
+              <LocationSearching className="userShowIcon" />
+              <span className="userShowInfoTitle">Abuja | Nigeria</span>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="userUpdate">
+          <span className="userUpdateTitle">Edit</span>
+          <form className="userUpdateForm">
+            <div className="userUpdateLeft">
+              <div className="userUpdateItem">
+                <label>Username</label>
+                <input
+                  type="text"
+                  placeholder="annabeck4"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  placeholder="Ann Becka"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Email</label>
+                <input
+                  type="text"
+                  placeholder="annabeck4@yahoo.com"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Phone</label>
+                <input
+                  type="text"
+                  placeholder="+1 686 8669 342"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Address</label>
+                <input
+                  type="text"
+                  placeholder="Abuja | Nigeria"
+                  className="userUpdateInput"
+                />
+              </div>
+            </div>
+            <div className="userUpdateRight">
+              <div className="userUpdateUpload">
+                <img
+                  className="userUpdateImg"
+                  src="https://www.pngfind.com/pngs/m/29-298536_anonymous-clipart-female-blank-profile-picture-girls-hd.png"
+                  alt=""
+                />
+                <label htmlFor="file"><Publish className="userUpdateIcon" /></label>
+                <input type="file" id="file" style={{display:"none"}}/>
+              </div>
+              <button className="userUpdateButton">Update</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default User
+export default User;
